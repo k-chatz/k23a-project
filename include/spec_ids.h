ef809bd77a46c5fad548c4710d38ea3a64d82c11
vasilisp100 <vasilis.p.100@gmail.com>
2020-11-01 21:28:11 +0200

    spec_ids     - Replace list with hash table     - Code cleanup
    Co-authored-by: Kostas Chatzopoulos <kwstarikanos@gmail.com>

--- include/spec_ids.h
+++ include/spec_ids.h
@@ -1,16 +1,8 @@
 #ifndef __PARSER_H__
 #define __PARSER_H__
 
-#include "lists.h"
+#include "spec_to_specs.h"
 
-typedef LISTOF(char*) StrList;
-
-StrList *create_node(char *spec);
-
-void print_list(StrList *list);
-
-void free_StrList_data(StrList *list);
-
-StrList *get_spec_ids();
+STS *get_spec_ids();
 
 #endif
