91120b47d83fb63c9e8e111cf62e595ddc620c7f
vasilisp100 <vasilis.p.100@gmail.com>
2020-11-01 21:29:25 +0200

    spec_to_specs_tests - Remove print_sts function
    Co-authored-by: Kostas Chatzopoulos <kwstarikanos@gmail.com>

--- tests/spec_to_specs_tests.c
+++ tests/spec_to_specs_tests.c
@@ -3,20 +3,7 @@
 
 #define N (sizeof(ids) / sizeof(ids[0]))
 
-void print_sts(STS *sts) {
-    StrList *keys = sts->keys;
-    while (keys) {
-        SpecEntry *sp = HT_Get(sts->ht, keys->data);
-        printf("%s -> (", sp->id);
-        SpecList *similar = sp->similar;
-        while (similar) {
-            printf("%s ", similar->data->id);
-            similar = llnth(similar, 1);
-        }
-        printf(")\n");
-        keys = llnth(keys, 1);
-    }
-}
+
 
 void add_test(void) {
     STS *sts = sts_new();
