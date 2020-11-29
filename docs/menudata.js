8b22d6d8923c937310694549d23e62eee96c0e40
Thodoris Chatziioannidis <hthodoris@gmail.com>
2020-11-29 23:40:40 +0200

    Added action to automatically create and publish docs

--- docs/menudata.js
+++ docs/menudata.js
@@ -31,6 +31,4 @@ var menudata={children:[
 {text:"Data Fields",url:"functions.html",children:[
 {text:"All",url:"functions.html"},
 {text:"Functions",url:"functions_func.html"},
-{text:"Variables",url:"functions_vars.html"}]}]},
-{text:"Files",url:"files.html",children:[
-{text:"File List",url:"files.html"}]}]}
+{text:"Variables",url:"functions_vars.html"}]}]}]}
