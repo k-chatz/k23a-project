4d25ad12db9067afaeac6e6e6b6743828ceec661
Kostas  Chatzopoulos <kwstarikanos@gmail.com>
2020-12-10 23:26:44 +0200

    ml tests: -created remove_stop_words test -reformat code
    Co-authored-by: vasilisp100 <vasilis.p.100@gmail.com>

--- tests/ml_tests.c
+++ tests/ml_tests.c
@@ -1,5 +1,6 @@
-#include "../include/acutest.h"
+//#include "../include/acutest.h"
 #include "../include/ml.h"
+
 #ifndef ACUTEST_H
 
 #include <assert.h>
@@ -9,25 +10,27 @@
 #endif
 
 void bag_of_words_(void) {
-    TEST_CHECK(1);
+            TEST_CHECK(1);
 }
 
-void rm_punctuations_and_replace_lower_case_with_upper_case(void) {
+void remove_punctuations_and_replace_lower_case_with_upper_case(void) {
     char buffer[128] = "tHe;-. a qui,,,.ck. 255 at 43 a fox.---- j to";
     rm_punct_and_upper_case(buffer);
-    TEST_CHECK(strcmp(buffer, "the a quick 255 at 43 a fox j to") == 0);
+            TEST_CHECK(strcmp(buffer, "the a quick 255 at 43 a fox j to") == 0);
 }
 
 
-
-void rm_stop_words_(void) {
-    char buffer[128] = "the a quick 255 at 43 a fox j to";
-    rm_stop_words(buffer);
-    TEST_CHECK(strcmp(buffer, "quick 255 43 fox j") == 0);
+void remove_stop_words(void) {
+    char buffer1[128] = "the a quick 255 at 43 a fox j f";
+    char buffer2[128] = "the a quick 255 at 43 a fox j at";
+    rm_stop_words(buffer1);
+    rm_stop_words(buffer2);
+            TEST_CHECK(strcmp(buffer1, "quick 255 43 fox j f") == 0);
+            TEST_CHECK(strcmp(buffer2, "quick 255 43 fox j") == 0);
 }
 
-void rm_digits_(void) {
-    TEST_CHECK(1);
+void remove_digits(void) {
+            TEST_CHECK(1);
 }
 
 #ifndef ACUTEST_H
@@ -42,10 +45,10 @@ struct test_ {
 #endif
 
 TEST_LIST = {
-        // {"bag_of_words", bag_of_words},
-        // {"rm_punctuations_and_replace_lower_case_with_upper_case", rm_punctuations_and_replace_lower_case_with_upper_case},
-        // {"rm_stop_words", rm_stop_words},
-        // {"rm_digits", rm_digits},
+        //{"bag_of_words", bag_of_words},
+        {"remove_punctuations_and_replace_lower_case_with_upper_case", remove_punctuations_and_replace_lower_case_with_upper_case},
+        {"remove_stop_words",                                          remove_stop_words},
+        {"remove_digits",                                              remove_digits},
         {NULL, NULL}
 };
 
