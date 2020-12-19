af9542577a71b2849c733959b243107bf0f76be6
Kostas  Chatzopoulos <kwstarikanos@gmail.com>
2020-12-19 23:13:34 +0200

    updated 'build_and_run.sh' script -used time
    Co-authored-by: vasilisp100 <vasilis.p.100@gmail.com>

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-make clean && make && ./project \
+make clean && make && time ./project \
 -dir Datasets/camera_specs/2013_camera_specs \
 -csv Datasets/sigmod_medium_labelled_dataset.csv \
 -sw resources/unwanted-words.txt
