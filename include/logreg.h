609f37c457add29beb3764c373713132acb1586d
Kostas  Chatzopoulos <kwstarikanos@gmail.com>
2020-12-25 14:22:14 +0200

    code reformatting
    Co-authored-by: vasilisp100 <vasilis.p.100@gmail.com>

--- include/logreg.h
+++ include/logreg.h
@@ -12,10 +12,17 @@ struct {
 } LogReg;
 
 LogReg *logreg_new(int weights_len, float learning_rate);
+
 void logreg_free(LogReg *reg);
+
 float sigmoid(float x);
+
 float sigmoid_grad(float x);
+
 float logloss(float p, bool y);
+
 float predict_once(LogReg *reg, float *X);
+
 float *predict(LogReg *reg, float *Xs, int batch_sz);
+
 float train(LogReg *reg, float *Xs, int *Ys, int batch_sz);
