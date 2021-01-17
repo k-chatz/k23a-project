614c243084fd7046cf0ad8bd9caf374d518aba28
Kostas  Chatzopoulos <kwstarikanos@gmail.com>
2021-01-17 07:38:14 +0200

    job_scheduler_tests was updated

--- tests/job_scheduler_tests.c
+++ tests/job_scheduler_tests.c
@@ -8,7 +8,7 @@
 pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t count_nonzero;
 
-unsigned long long int count = 0;
+unsigned long long int sum = 0;
 
 JobScheduler js = NULL;
 
@@ -18,10 +18,10 @@ clock_t begin, end;
 /*from: https://stackoverflow.com/questions/27349480/condition-variable-example-for-pthread-library*/
 void *decrement(Job job) {
     pthread_mutex_lock(&mtx);
-    while (count == 0) {
+    while (sum == 0) {
         pthread_cond_wait(&count_nonzero, &mtx);
     }
-    count = count - 1;
+    sum = sum - 1;
     pthread_mutex_unlock(&mtx);
     return NULL;
 }
@@ -30,55 +30,103 @@ void *increment(Job job) {
     //int sec = rand() % 2 + 1;
     //sleep(1);
     pthread_mutex_lock(&mtx);
-    if (count == 0) {
+    if (sum == 0) {
         pthread_cond_signal(&count_nonzero);
     }
-    count = count + 1;
-    printf(CYAN"[%ld] job %lld, count:%lld\n"RESET, pthread_self(), job->job_id, count);
+    sum = sum + 1;
+    printf(CYAN"[%ld] job %lld, sum:%lld\n"RESET, pthread_self(), job->job_id, sum);
     pthread_mutex_unlock(&mtx);
     return NULL;
 }
 
+/*from: https://github.com/kwstarikanos/ip-smith-numbers*/
+int sd(int x) {
+    int sum = 0, Digit = 0;
+    while (x > 0) {
+        Digit = x % 10;
+        x /= 10;
+        sum += Digit;
+    }
+    return sum;
+}
+
+void *smith_numbers(Job job) {
+    int i = 0, x = 0, y = 0, z = 0, sumfact = 0, smith = 0;
+    long timesec = 0;
+    timesec = time(NULL);
+    srand((unsigned int) timesec);
+    do {
+        y = rand();
+        z = rand();
+        x = ((y % 32768) + 1) * ((z % 32768) + 1) + 1;
+        sumfact = 0;
+        z = x;
+        for (y = 2; y * y <= z;) {
+            if (z % y == 0) {
+                sumfact += sd(y);
+                z /= y;
+            } else {
+                if (y > 2)
+                    y += 2;
+                else
+                    y = 3;
+            }
+        }
+        if (z == 1)
+            z = 0;
+        if (z != 0)
+            sumfact += sd(z);
+        if (z == x)
+            sumfact = 0;
+        if (sumfact == sd(x)) {
+            smith++;
+            //printf("%10d is Smith number\n", x);
+            pthread_mutex_lock(&mtx);
+            if (sum == 0) {
+                pthread_cond_signal(&count_nonzero);
+            }
+            sum += x;
+            pthread_mutex_unlock(&mtx);
+        }
+        i++;
+    } while (i <= *(int *) job->arg);
+    //printf(CYAN"Thread [%ld] job %lld, Found %4.2f%% Smith numbers sum:%lld\n"RESET, pthread_self(), job->job_id, (100.0 * smith) / i, sum);
+    return NULL;
+}
+
+
 void create_job_scheduler(void) {
-    putchar('\n');
     begin = clock();
-    js_create(&js, 50);
+    js_create(&js, 40);
     TEST_CHECK(js != NULL);
 }
 
 void submit_jobs(void) {
-    putchar('\n');
+    int computations = 10000;
     for (int j = 0; j < 356; ++j) {
-        Job job = js_create_job((void *(*)(void *)) increment, NULL);
+        Job job = js_create_job((void *(*)(void *)) smith_numbers, &computations, sizeof(computations));
         //Job job = js_create_job((void *(*)(void *)) decrement, NULL);
         TEST_CHECK(js_submit_job(js, job));
     }
 }
 
 void execute_all_jobs(void) {
-    putchar('\n');
     TEST_CHECK(js_execute_all_jobs(js));
 }
 
 void overflow_job_scheduler(void) {
-    putchar('\n');
     for (int j = 0; j < 0; ++j) {
         //printf(RED"inserting job...\n"RESET);
-        Job job = js_create_job((void *(*)(void *)) increment, NULL);
+        Job job = js_create_job((void *(*)(void *)) increment, NULL, 0);
         TEST_CHECK(js_submit_job(js, job));
         //printf(RED"OK!\n"RESET);
     }
 }
 
 void wait_all_jobs(void) {
-    putchar('\n');
     TEST_CHECK(js_wait_all_jobs(js));
-    printf(UNDERLINE BOLD"count: %lld\n"RESET, count);
-    //TEST_CHECK(count == 0);
-
-    end = clock();
-    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-    printf("time spend: %f\n", time_spent);
+    printf(UNDERLINE BOLD"sum: %lld\n"RESET, sum);
+    printf("time spend: %f\n", (double) (clock() - begin) / CLOCKS_PER_SEC);
 }
 
 void destroy_job_scheduler(void) {
