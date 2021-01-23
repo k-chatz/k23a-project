3abb1e7a5ecb24c4fcf15310cab9ef621694c99f
vasilisp100 <vasilis.p.100@gmail.com>
2021-01-23 03:49:14 +0200

    updated build_and run script and added new model
    Co-authored-by: Kostas Chatzopoulos <kwstarikanos@gmail.com>

--- build_and_run.sh
+++ build_and_run.sh
@@ -2,14 +2,9 @@
 
 make clean
 make
+#valgrind --vgdb=yes --vgdb-error=0 
+./project -dir Datasets/camera_specs/2013_camera_specs -csv Datasets/sigmod_large_labelled_dataset.csv -ex resources -m tfidf -sw resources/unwanted-words.txt
 
-#valgrind --vgdb=yes --vgdb-error=0 ./project \
-./project \
--dir Datasets/camera_specs/2013_camera_specs \
--csv Datasets/sigmod_large_labelled_dataset.csv \
--sw resources/unwanted-words.txt \
--m bow \
--ex /home/msi/CLionProjects/k23a-project/resources
 
 echo "Running user main..."
 
