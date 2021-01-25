d14f0c7445c76635e294f8bb720490234d840534
vasilisp100 <vasilis.p.100@gmail.com>
2021-01-26 00:39:17 +0200

    added a valgrind command in build_and_run.sh
    Co-authored-by: Kostas Chatzopoulos <kwstarikanos@gmail.com>

--- build_and_run.sh
+++ build_and_run.sh
@@ -3,6 +3,7 @@
 make clean
 make
 #valgrind --vgdb=yes --vgdb-error=0 
+#valgrind -s --leak-check=full --show-leak-kinds=all  
 ./project -dir Datasets/camera_specs/2013_camera_specs -csv Datasets/sigmod_large_labelled_dataset.csv -ex resources -m tfidf -sw resources/unwanted-words.txt
 
 
